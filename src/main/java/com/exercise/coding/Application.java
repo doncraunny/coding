3ab288e950dc313940a099e4005b4a2c4617ef27
Donald Nnama <donald.macbraun@paconsulting.com>
2020-09-24 19:24:16 +0100

    integrating swagger api doc

--- src/main/java/com/exercise/coding/Application.java
+++ src/main/java/com/exercise/coding/Application.java
@@ -2,12 +2,33 @@ package com.exercise.coding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
+    @Bean
+    public Docket getDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.exercise.coding"))
+                .paths(PathSelectors.regex("/api/.*"))
+                .build()
+                .apiInfo(new ApiInfoBuilder()
+                        .title("Transaction Web API Doc")
+                        .description("Transaction Web API Doc")
+                        .version("1.0")
+                        .build());
+    }
 }
